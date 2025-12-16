c7ec0eb162a1957c3b1300101f67ba40be160e7c
stanenbaum <96847244+stanenbaum@users.noreply.github.com>
2025-12-16 17:51:37 -0500

    Coursera lab Linux DevOps io sheild

--- run-as-coder.sh
+++ run-as-coder.sh
@@ -1,2 +1,2 @@
 # Add any commands or scripts that you wish to run as coder here
-/usr/bin/x-terminal-emulator
+/usr/bin/google-chrome --no-sandbox --test-type --disable-features=PrivacySandboxSettings4 --disable-dev-shm-usage /home/coder/coursera/ioredirection.jpg
